a11e2c97d62376b711f5fb04ad15a06dad9d576f
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-07 14:11:40 -0500

    create a movie model

---
+++ src/main/java/com/example/movieApp/models/Movie.java
@@ -0,0 +1,68 @@
+package com.example.movieApp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private int year;
+    private double rating;
+
+    public Movie() {
+    }
+
+    public Movie(String title, int year, double rating, String description) {
+        this.title = title;
+        this.year = year;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

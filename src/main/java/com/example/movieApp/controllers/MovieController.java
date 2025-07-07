bc017a0fe368243f6ba6384a7b398c4ed2e88832
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-07 14:28:02 -0500

    create methods for getting all the movies and adding a new movie to the movie controller class

--- src/main/java/com/example/movieApp/controllers/MovieController.java
+++ src/main/java/com/example/movieApp/controllers/MovieController.java
@@ -1,4 +1,30 @@
 package com.example.movieApp.controllers;
 
+import com.example.movieApp.models.Movie;
+import com.example.movieApp.repositories.MovieRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return ResponseEntity.status(HttpStatus.OK).body(movieRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie newMovie) {
+        // generate the description for the movie here
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieRepository.save(newMovie));
+    }
 }
